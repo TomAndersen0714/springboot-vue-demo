3e44260729f3d94800ba1a203464893ef990bea9
TomAndersen <1040994898@qq.com>
2023-03-14 23:32:31 +0800

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <Header/>
+    <!--    <router-link to="/">Home</router-link> |-->
+    <!--    <router-link to="/about">About</router-link>-->
+
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "Layout",
+  components: {
+    Header
+  }
+}
+</script>
